d23ae206365c47d34a76df1b36a65309d711febb
John Shelburne <john@catfix.biz>
2024-08-12 13:45:46 -0500

    Using Sourcery

--- update_post_dates.rb
+++ update_post_dates.rb
@@ -14,6 +14,18 @@ Dir.glob("#{posts_dir}/*.md").each do |file|
     body = $POSTMATCH
 
     # Parse YAML front matter with safe_load and permitted_classes
++    body = $POSTMATCH
++
++    # Parse YAML front matter with safe_load and permitted_classes
++    data = YAML.safe_load(front_matter, permitted_classes: [Date, Time])
++
++    # Update date format if date exists
+ ## It will be a page on the interwebs using Github Actions and Github Codespaces and Github Pages <br>
+ 
+ <img src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTI5bDB2eDI1dHg1amMzNXlndGVuN2MzanNxdTlsa2owM3lwNmNrciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2R7wTPROGOSn6/giphy.gif" width="50%"><br>
+-## Hopefully linking it up to a custom domain.....i know its aggressive<br>
+-<img src ="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXU0Z2NudDF4eWxpODRxZmphcjMxMzNyOW4zcnBvbG42ZXJ1NjhmeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Aff4ryYiacUO4/giphy.gif">
+ 
     data = YAML.safe_load(front_matter, permitted_classes: [Date, Time])
 
     # Update date format if date exists
